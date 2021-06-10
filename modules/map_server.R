309f9983b8b0fdba98e093098c30bdd904fa59ae
Raphael Saldanha <rfsaldanha@gmail.com>
2021-06-10 10:45:26 -0300

    Map pane adapted to panel.

--- modules/map_server.R
+++ modules/map_server.R
@@ -1,17 +1,17 @@
 output$map_variable_UI <- renderUI({
   req(geodata())
-  sub <- geodata()@data %>%
+  sub <- geodata_tmp()@data %>%
     select_if(is.numeric)
   varSelectInput("map_variable", label = "Variable", data = sub)
 })
 
 
 output$map_leaflet <- renderLeaflet({
-  req(geodata())
+  req(geodata_tmp())
   req(input$map_variable)
   
   map_var <- as.character(input$map_variable)
-  map <- tm_shape(geodata()) +
+  map <- tm_shape(geodata_tmp()) +
     tm_fill(col = map_var,
             style = as.character(input$map_style),
             alpha = 0.7,
