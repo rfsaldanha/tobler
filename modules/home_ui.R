760eab50f438e93530d4099fa50e9def6b183178
Raphael Saldanha <rfsaldanha@gmail.com>
2021-06-23 20:15:06 -0300

    Home pane new text.

--- modules/home_ui.R
+++ modules/home_ui.R
@@ -18,7 +18,7 @@ home_tab <- argonTabItem(
           floating = TRUE,
           card_mode = FALSE
         ),
-        p("Tobler is a web app that offers a intuitive step-by-step process for estimation of spatial econometric models, including a basic spatial visualization, creating of spatial weighting matrix and spatial correlation tests. Currently, it is possible to estimate OLS, SAR, SEM, SAC, SLX, SDM and SDEM models, with maximum likelihood and IV/GMM estimators. Estimates of impacts are provided, when applicable."),
+        p("Tobler is a web app that offers a intuitive step-by-step process for estimation of spatial econometric models, including a basic spatial visualization, creating of spatial weighting matrix and spatial correlation tests. Currently, it is possible to estimate several cross-section and panel models, with different estimators. Estimates of impacts are provided, when applicable."),
         p("This app was entirely created with R using the Shiny library. Spatial data is handled by the RGDAL, cleangeo and leaflet libraries. Spatial dependence tests and spatial models are provided by the spdep and spatialreg library."),
         p('The name is a tribute to the geographer Waldo R. Tobler and his First Law of Geography: "Everything is related to everything else, but near things are more related than distant things."'),
         p("This is a prototype. All results must be checked and validated by the users."),
@@ -27,7 +27,7 @@ home_tab <- argonTabItem(
       argonCard(
         width = 12,
         src = NULL,
-        #icon = icon("cogs"),
+        icon = argonIcon("atom"),
         status = "success",
         shadow = TRUE,
         border_level = 2,
