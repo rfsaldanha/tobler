fba7d8da09cf882899fb995a8534f0c4b5fcc321
Raphael Saldanha <rfsaldanha@gmail.com>
2021-07-06 18:35:01 -0300

    Includes sphet library in the text.

--- modules/home_ui.R
+++ modules/home_ui.R
@@ -19,7 +19,7 @@ home_tab <- argonTabItem(
           card_mode = FALSE
         ),
         p("Tobler is a web app that offers a intuitive step-by-step process for estimation of spatial econometric models, including a basic spatial visualization, creating of spatial weighting matrix and spatial correlation tests. Currently, it is possible to estimate several cross-section and panel models, with different estimators. Estimates of impacts are provided, when applicable."),
-        p("This app was entirely created with R using the Shiny library. Spatial data is handled by the RGDAL, cleangeo and leaflet libraries. Spatial dependence tests and spatial models are provided by the spdep, spatialreg and splm library."),
+        p("This app was entirely created with R using the Shiny library. Spatial data is handled by the RGDAL, cleangeo and leaflet libraries. Spatial dependence tests and spatial models are provided by the spdep, spatialreg, sphet and splm library."),
         p('The name is a tribute to the geographer Waldo R. Tobler and his First Law of Geography: "Everything is related to everything else, but near things are more related than distant things."'),
         p("This is a prototype. All results must be checked and validated by the users."),
         p("Developers: Raphael Saldanha (Fiocruz); Eduardo Almeida (UFJF).")
